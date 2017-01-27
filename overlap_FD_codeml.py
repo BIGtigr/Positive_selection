01681992ded330e569a7829e03de051a479bd13e
lauguma <laugmacias@gmail.com>
2017-01-27 10:45:13 +0100

    Update overlap_FD_codeml.py

--- overlap_FD_codeml.py
+++ overlap_FD_codeml.py
@@ -3,6 +3,12 @@
 import sys
 import os
 
+def commonElements(list_a, list_b):
+    common = list(set(list_a).intersection(list_b))
+    
+    return common
+
+
 def main():
     # List both FD codeml files
     path_fd = os.path.dirname(sys.argv[1])
@@ -11,31 +17,32 @@ def main():
     pv_files = [z for z in os.listdir(path_pv) if z.endswith("_alt.txt") and z.startswith("Y")]
 
 
-    fd = dict()
+    fd = dict()# KEY: gene name VALUE: positions changed by functional divergence
     # read number of changes in FD files
     for f in fd_files:
         gene = f.split(".")[0]
-        c = 0
-        pos_fd = []
+        fd.setdefault(gene,[])
+        pos_fd = [] 
         with open(os.path.join(path_fd,f),"r") as f_in:
             for line in f_in:
                 if "p <" in line:
                     line = line.replace("\n","")
-                    pos_fd.append(line.split("\t")[0])
-        fd.setdefault(gene,[])
+                    pos_fd.append(line.split("\t")[0]) # add position values to array
+        
         for p in pos_fd:
             p = p.replace(" ","")
-            fd[gene].append(p)
+            fd[gene].append(p) # add position values to dictionary with gene name as key
         if len(fd) == 0:
-            fd[gene].append("0") # anhadir 0 al diccionario si no hay sitios de divergencia funcional
+            fd[gene].append("0") # add value zero if there is no FD change in that protein
             
-        print fd
+    
 
 
     sel = dict() # KEY: gene name VALUE: positions under positive selection
     for h in pv_files:
-        pos_pv = []
         gene = h.split("_")[0]
+        sel.setdefault(gene,[])
+        pos_pv = []
         i = True
         # Read specific codeml output until find BEB results
         with open(os.path.join(path_pv,h),"r") as f_in: 
@@ -44,24 +51,41 @@ def main():
                     line = f_in.next()
                     if line != "\n":
                         while i:
-                            pos_pv.append(line)
+                            pos_pv.append(line) # add specific lines to matrix
                             line = f_in.next()
                             if line == "\n":
-                                i = False
+                                i = False  # stop reading when there is a \n
 
 
         res = []
-        sel.setdefault(gene,[])
-        for x in pos_pv:
+        for x in pos_pv:   # format position values, keeping only integers from line
             x = x.replace("\n","")
             x = x.replace("    ","")
             x = x.replace("   ","")
             res.append(x.split(" ")[0])
 
         for r in res:
-            sel[gene].append(r)
+            sel[gene].append(r) # add position values to dictionary with gene name as key
 
-        print sel
+
+    for k in sel.keys():
+        val_sel = []
+        val_fd = []
+        common = []
+        if k in fd.keys():
+            val_sel = sel.get(k)
+            val_fd = fd.get(k)
+
+        common = commonElements(val_sel,val_fd)
+        if len(common) != 0:
+            with open("overlapping_FD.csv","a") as f_out:
+                f_out.write(k+",")
+                for val in common:
+                    f_out.write(str(val)+",")
+                f_out.write("\n")
+
+            
+
 
 
                     
@@ -70,3 +94,4 @@ def main():
 
 if __name__ == "__main__" :
     main()
+
